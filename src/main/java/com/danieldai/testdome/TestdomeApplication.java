15f54aa23d259c8fd5d7b65d413d5b5a367b0637
Daniel Dai <daeming@gmail.com>
2024-10-14 20:46:11 +0800

    start project

---
+++ src/main/java/com/danieldai/testdome/TestdomeApplication.java
@@ -0,0 +1,13 @@
+package com.danieldai.testdome;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestdomeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestdomeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/danieldai/testdome/TestdomeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.danieldai.testdome;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestdomeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
